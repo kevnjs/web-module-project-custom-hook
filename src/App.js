6bec0627d817961b9348bb97db60bc9dce1d4e90
kevnjs <91511588+kevnjs@users.noreply.github.com>
2022-01-12 16:31:21 -0600

    Finished app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
-
 import "./style.css";
+import useTracker from "./hooks/useTracker";
+
+//- Build custom hooks that extend stateful logic +
+// - Build custom hooks that work with persistent data 
+// - Compose hooks together
+
+const useLocalStorage = (key, initValue) => {
+  const [value, setValue ] = useState(() => {
+    
+    if(localStorage.getItem(key)) {
+      return(JSON.parse(localStorage.getItem(key)))
+    } else {
+      localStorage.setItem(key, JSON.stringify(initValue))
+      return (initValue);
+    }
+  })
+
+  const setLocalStorage = (newValue) => {
+    localStorage.setItem(key, JSON.stringify(newValue))
+    setValue(newValue);
+  }
+
+  return [value, setLocalStorage];
+}
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useLocalStorage("mode", false);
+  const [ coinData ] = useTracker();
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
